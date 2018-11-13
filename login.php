57159e302f2e0496251ad8dba5e467463acc1ef6
ahsanriyad <riyad298@gmail.com>
2018-11-13 15:26:51 +0600

    login complete

--- login.php
+++ login.php
@@ -2,77 +2,90 @@
 session_start();
 
 if (isset($_POST['submit'])) {
-    $file = fopen("user_info.txt", "r");
 
-    while (!feof($file)) {
+    $id = $_POST['id'];
 
-        $GLOBALS['fileArray'] = explode('|', fgets($file));
-        if ($fileArray[0] == $_POST['username']) {
+    $conn = mysqli_connect('localhost', 'root', '', 'wt');
 
-            if ($fileArray[1] == $_POST['password']) {
+    $sql = "select * from user where id='$id' and password='".$_POST['password']."'";
+    //echo $sql;
 
-                $_SESSION['user_name_s'] = $_POST['username'];
-                $_SESSION['passoword_s'] = $_POST['password'];
-                $_SESSION['email_s'] = trim($fileArray[3]);
-                $_SESSION['type_p'] = trim($fileArray[4]);
-                $_SESSION['user_id_s'] =  trim($fileArray[2]);
+    $result = mysqli_query($conn, $sql);
 
-                //print_r($_SESSION);
-                
-                if (trim($fileArray[4]) == 'User') {
-                    header('Location: user_home.php');
-                    break;
-                } else if (trim($fileArray[4]) == 'Admin') {
-                    header('Location: admin_home.php');
-                    break;
-                } else {
-                    echo 'something went wrong';
-                    echo $fileArray[4];
-                    break;
-                }
-            }
+    $row = mysqli_fetch_assoc($result);
+
+    mysqli_close($conn);
+
+
+
+
+
+
+    $_SESSION['user_name_s'] = $_POST['id'];
+    $_SESSION['passoword_s'] = $_POST['password'];
+    $_SESSION['email_s'] = $row['email'];
+    $_SESSION['type_p'] = $row['type'];
+    $_SESSION['user_id_s'] = $row['id'];
+
+
+    if($_POST['id'] == $row['id']  &&  $_POST['password'] == $_SESSION['passoword_s'])
+    {
+        if (trim($_SESSION['type_p']) == 'User') {
+            header('Location: user_home.php');
+            
+        } else if (trim($_SESSION['type_p']) == 'Admin') {
+            header('Location: admin_home.php');
+            
+        } else {
+            echo 'something went wrong';
+            
         }
     }
-
-    fclose($file);
+    else {
+        echo 'pass does not match';
+    }
 }
+
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Login</title>
-    </head>
-    <body>
+<head>
+    <title>Login</title>
+</head>
+<body>
 
-        <form method="post" action="#">
-            <fieldset>
-                <legend>LOGIN</legend>
-                <table>
-                    <tr>
-                        <td>USERNAME</td>
-                        <td><input type="text" name="username"></td>
-                    </tr>
-                    <tr>
-                        <td>PASSWORD</td>
-                        <td><input type="password" name="password"></td>
-                    </tr>
-                    <tr>
-                        <td>Remember Me</td>
-                        <td><input type="checkbox" name="remember">
-                            <a href="reg.php">Register here</a>
+    <form method="post" action="#">
+        <fieldset>
+            <legend>LOGIN</legend>
+            <table>
+                <tr>
+                    <td>USERNAME</td>
+                    <td><input type="text" name="id"></td>
+                </tr>
+                <tr>
+                    <td>PASSWORD</td>
+                    <td><input type="password" name="password"></td>
+                </tr>
+                <tr>
+                    <td>Remember Me</td>
+                    <td><input type="checkbox" name="remember">
+                        <a href="reg.php">Register here</a>
 
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type="submit" name="submit" value="Submit"></td>
-                    </tr>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="submit" value="Submit"></td>
+                </tr>
 
 
 
-                </table>
-            </fieldset>
-        </form>
-    </body>
+            </table>
+        </fieldset>
+    </form>
+</body>
 </html>
